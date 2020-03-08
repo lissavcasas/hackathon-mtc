a546c3ed73673de030012e52fad54732a69c3cfa
marimeli <marimel.casas@gmail.com>
2020-03-07 21:31:50 -0500

    add map dummy

--- lib/pages/sos_page.dart
+++ lib/pages/sos_page.dart
@@ -9,34 +9,43 @@ class SOSPage extends StatefulWidget {
 class _SOSPageState extends State<SOSPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ClipOval(
-        child: Material(
-          color: const Color(0xFFD40C16), // button color
-          child: InkWell(
-            splashColor: Colors.redAccent, // inkwell color
-            child: SizedBox(
-              width: 56,
-              height: 56,
-              child: Center(
-                child: Text(
-                  'SOS',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-            onTap: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (BuildContext context) {
-                return CreateAlertPage();
-              }));
-            },
+    return Stack(
+      children: <Widget>[
+        Center(
+          child: Image.asset(
+            "assets/images/map.png",
           ),
         ),
-      ),
+        Center(
+          child: ClipOval(
+            child: Material(
+              color: const Color(0xFFD40C16), // button color
+              child: InkWell(
+                splashColor: Colors.redAccent, // inkwell color
+                child: SizedBox(
+                  width: 56,
+                  height: 56,
+                  child: Center(
+                    child: Text(
+                      'SOS',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                onTap: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (BuildContext context) {
+                    return CreateAlertPage();
+                  }));
+                },
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
